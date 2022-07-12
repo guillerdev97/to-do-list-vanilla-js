61cc2a73cd07984a05156cf540f46d58fcb164b1
Guillermo García <guillermieres97@gmail.com>
2022-07-12 09:01:41 +0200

    working on localstorage

--- js/index.js
+++ js/index.js
@@ -1,1 +1,54 @@
 "use strict"
+
+const showTaskSection = document.querySelector(".showTaskSection");
+const btnTask = document.getElementById("btnTask");
+
+btnTask.addEventListener("click", addTaskToArray);
+
+let myTasks = [];
+addTaskToLocalStorage();
+
+/* checkLocalStorage(); */
+function checkLocalStorage() {
+    if(typeof(Storage) != "undefined") {
+        console.log("LocalStorage is available");
+    }
+    if(typeof(Storage) === "undefined") {
+        console.log("LocalStorage isn't available");
+    }
+}
+
+
+
+
+
+
+function addTaskToArray() {
+    const newTask = document.getElementById("task").value;
+    if(myTasks.includes(newTask) === false) {
+        myTasks.push(newTask);
+    }
+}
+function addTaskToLocalStorage() {
+    if(myTasks.length > 0) {
+        localStorage.setItem("tasks", JSON.stringify(myTasks));
+        const storedTasks = JSON.parse(localStorage.getItem("tasks"));
+        storedTasks.forEach(function(eachTask) {
+            showTask(eachTask);
+        })
+    }
+}
+
+
+
+function showTask(item) {
+    const divTask = document.createElement("div");
+    const text = item;
+    divTask.append(text);
+    showTaskSection.appendChild(divTask);
+}
+
+
+
+
+/* localStorage.clear(); */
